e1b78d4b3f02588f820145cba4b6c92b5ba108bd
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-05 07:24:25 +0000

    set output_dir

--- run/generate_rearrange.sh
+++ run/generate_rearrange.sh
@@ -10,10 +10,11 @@ config="../config/rearrange/diffusion_bedrooms_instancond_lat32_v_rearrange.yaml
 exp_name="bedrooms_rearrange"
 weight_file=$exp_dir/$exp_name/model_17000
 threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_bedroom.pkl'
+output_dir=./cluster/balrog/jtang/rearrange/$exp_name/gen_top2down_notexture_nofloor
 
 xvfb-run -a python completion_rearrange.py \
     $config \
-    $exp_dir/$exp_name/gen_top2down_notexture_nofloor \
+    $output_dir \
     $threed_future \
     --weight_file $weight_file \
     --without_screen \
@@ -32,10 +33,11 @@ xvfb-run -a python completion_rearrange.py \
 # exp_name="livingrooms_rearrange"
 # weight_file=$exp_dir/$exp_name/model_81000
 # threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_livingroom.pkl'
+# output_dir=./cluster/balrog/jtang/rearrange/$exp_name/gen_top2down_notexture_nofloor
 
 # xvfb-run -a python completion_rearrange.py \
 #     $config \
-#     $exp_dir/$exp_name/gen_top2down_notexture_nofloor \
+#     $output_dir \
 #     $threed_future \
 #     --weight_file $weight_file \
 #     --without_screen \
