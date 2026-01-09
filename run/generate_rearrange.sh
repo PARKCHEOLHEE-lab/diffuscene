e3a4ede8bf65a8cb8f380ff1be3446e662bb26ea
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-09 08:19:11 +0000

    generate_rearrange.sh

--- run/generate_rearrange.sh
+++ run/generate_rearrange.sh
@@ -10,7 +10,7 @@ config="../config/rearrange/diffusion_bedrooms_instancond_lat32_v_rearrange.yaml
 exp_name="bedrooms_rearrange"
 weight_file=$exp_dir/$exp_name/model_17000
 threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_bedroom.pkl'
-output_dir=./cluster/balrog/jtang/rearrange_with_train_val_data/$exp_name/gen_top2down_notexture_nofloor
+output_dir=./cluster/balrog/jtang/rearrange_with_validation_data/$exp_name/gen_top2down_notexture_nofloor
 
 xvfb-run -a python completion_rearrange.py \
     $config \
@@ -27,14 +27,42 @@ xvfb-run -a python completion_rearrange.py \
     --retrive_objfeats \
     --arrange_objects \
     --compute_intersec \
-    --use_test_dataset=false
+    --split='["val"]' \
+    --render_gt=true
 
-# ####'livingrooms'
-# config="../config/rearrange/diffusion_livingrooms_instancond_lat32_v_rearrange.yaml"
-# exp_name="livingrooms_rearrange"
-# weight_file=$exp_dir/$exp_name/model_81000
-# threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_livingroom.pkl'
-# output_dir=./cluster/balrog/jtang/rearrange/$exp_name/gen_top2down_notexture_nofloor
+####'livingrooms'
+config="../config/rearrange/diffusion_livingrooms_instancond_lat32_v_rearrange.yaml"
+exp_name="livingrooms_rearrange"
+weight_file=$exp_dir/$exp_name/model_81000
+threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_livingroom.pkl'
+output_dir=./cluster/balrog/jtang/rearrange_with_validation_data/$exp_name/gen_top2down_notexture_nofloor
+
+xvfb-run -a python completion_rearrange.py \
+    $config \
+    $output_dir \
+    $threed_future \
+    --weight_file $weight_file \
+    --without_screen \
+    --n_rearrange_times 1 \
+    --render_top2down \
+    --no_texture \
+    --without_floor \
+    --save_mesh \
+    --clip_denoised \
+    --retrive_objfeats \
+    --arrange_objects \
+    --compute_intersec \
+    --split='["val"]' \
+    --render_gt=true
+
+
+# NOTE: there is no model checkpoint for diningrooms rearrangement
+# ####'diningrooms'
+# config="../config/rearrange/diffusion_diningrooms_instancond_lat32_v_rearrange.yaml"
+# exp_name="diningrooms_rearrange"
+# weight_file=$exp_dir/$exp_name/???  
+# threed_future='./cluster/balrog/jtang/3d_front_processed/threed_future_model_diningroom.pkl'
+# output_dir=./cluster/balrog/jtang/rearrange_with_validation_data/$exp_name/gen_top2down_notexture_nofloor
 
 # xvfb-run -a python completion_rearrange.py \
 #     $config \
@@ -51,4 +79,5 @@ xvfb-run -a python completion_rearrange.py \
 #     --retrive_objfeats \
 #     --arrange_objects \
 #     --compute_intersec \
-#     --use_test_dataset=false
+#     --split='["val"]' \
+#     --render_gt=true
