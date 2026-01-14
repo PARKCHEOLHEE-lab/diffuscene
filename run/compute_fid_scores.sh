e6b9fd591782c8eb09c7e20c2c92d700b332386f
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-14 01:34:43 +0000

    rearrange_with_train_val_data, clip, inception

--- run/compute_fid_scores.sh
+++ run/compute_fid_scores.sh
@@ -1,32 +1,60 @@
 #!/bin/bash
 cd ./scripts
 
-echo "Computing FID scores with global rotation checking for bedrooms rearrangement..."
+echo "Computing FID scores with clip and global rotation checking for bedrooms rearrangement..."
 python compute_fid_scores.py \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
     --feature_extractor=clip \
     --check_global_rotation=true
 
-echo "Computing FID scores without global rotation checking for bedrooms rearrangement..."
+echo "Computing FID scores with clip and without global rotation checking for bedrooms rearrangement..."
 python compute_fid_scores.py \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
     --feature_extractor=clip \
+    --check_global_rotation=false
+
+echo "Computing FID scores with inception and global rotation checking for bedrooms rearrangement..."
+python compute_fid_scores.py \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    --feature_extractor=inception \
+    --check_global_rotation=true
+
+echo "Computing FID scores with inception and without global rotation checking for bedrooms rearrangement..."
+python compute_fid_scores.py \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/bedrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    --feature_extractor=inception \
     --check_global_rotation=false
 
 echo ""
 
-echo "Computing FID scores with global rotation checking for livingrooms rearrangement..."
+echo "Computing FID scores with clip and global rotation checking for livingrooms rearrangement..."
 python compute_fid_scores.py \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
     --feature_extractor=clip \
     --check_global_rotation=true
 
-echo "Computing FID scores without global rotation checking for livingrooms rearrangement..."
+echo "Computing FID scores with clip and without global rotation checking for livingrooms rearrangement..."
 python compute_fid_scores.py \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
-    ./cluster/balrog/jtang/rearrange_with_validation_data_1/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
     --feature_extractor=clip \
+    --check_global_rotation=false
+
+echo "Computing FID scores with inception and global rotation checking for livingrooms rearrangement..."
+python compute_fid_scores.py \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    --feature_extractor=inception \
+    --check_global_rotation=true
+
+echo "Computing FID scores with inception and without global rotation checking for livingrooms rearrangement..."
+python compute_fid_scores.py \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/groundtruth \
+    ./cluster/balrog/jtang/rearrange_with_train_val_data/livingrooms_rearrange/gen_top2down_notexture_nofloor/generated \
+    --feature_extractor=inception \
     --check_global_rotation=false
