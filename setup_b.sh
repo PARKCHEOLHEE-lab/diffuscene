dbc54a884440f4593b9e78950557cb386dcc5bb8
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-15 01:25:51 +0000

    highlight

--- setup_b.sh
+++ setup_b.sh
@@ -6,35 +6,35 @@
 # 3d_front_processed.zip
 gdown 1UNSFN0kULyOzUErDPVvkKYbmfzA-4MsG
 if [ $? -ne 0 ]; then
-    echo ""
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
     echo "Download failed."
-    echo "Please download the file manually from https://drive.usercontent.google.com/download?id=1UNSFN0kULyOzUErDPVvkKYbmfzA-4MsG&authuser=0"
-    exit 1
+    echo "Please DOWNLOAD this file MANUALLY from https://drive.usercontent.google.com/download?id=1UNSFN0kULyOzUErDPVvkKYbmfzA-4MsG&authuser=0"
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
 fi
 
 # objautoencoder_pretrained.zip
 gdown 1xuFa_Hh6BOZqfaAnlG-W_9mA1gilU6nI
 if [ $? -ne 0 ]; then
-    echo ""
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
     echo "Download failed."
-    echo "Please download the file manually from https://drive.usercontent.google.com/download?id=1xuFa_Hh6BOZqfaAnlG-W_9mA1gilU6nI&authuser=0"
-    exit 1
+    echo "Please DOWNLOAD this file MANUALLY from https://drive.usercontent.google.com/download?id=1xuFa_Hh6BOZqfaAnlG-W_9mA1gilU6nI&authuser=0"
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
 fi
 
 # pretrained_diffusion.zip
 gdown 1pk9AzGcBz_kRfmRzvFNDW5byk4MwbXEm
 if [ $? -ne 0 ]; then
-    echo ""
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
     echo "Download failed."
-    echo "Please download the file manually from https://drive.usercontent.google.com/download?id=1pk9AzGcBz_kRfmRzvFNDW5byk4MwbXEm&authuser=0"
-    exit 1
+    echo "Please DOWNLOAD this file MANUALLY from https://drive.usercontent.google.com/download?id=1pk9AzGcBz_kRfmRzvFNDW5byk4MwbXEm&authuser=0"
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
 fi
 
 # 3D-FUTURE-model-processed.zip
 gdown 16fz81Eh6B6pbZNGTMbe0FdfNwIuQicE1
 if [ $? -ne 0 ]; then
-    echo ""
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
     echo "Download failed."
-    echo "Please download the file manually from https://drive.usercontent.google.com/download?id=16fz81Eh6B6pbZNGTMbe0FdfNwIuQicE1&authuser=0"
-    exit 1
+    echo "Please DOWNLOAD this file MANUALLY from https://drive.usercontent.google.com/download?id=16fz81Eh6B6pbZNGTMbe0FdfNwIuQicE1&authuser=0"
+    echo "---------------------------------------------------------------------------------------------------------------------------------------"
 fi
